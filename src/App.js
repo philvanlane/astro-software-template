4022d09a8d970ecfb3498136b4a7f8f7f0b9f9ec
Phil Van-Lane <philv19@gmail.com>
2025-05-09 14:27:46 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
         Hi! I'm a web app! I do things!
       </header>
       <div className='app-body'>
-      You can see my source code <a href="https://github.com/philvanlane/astro-software-template" target="_blank" rel="noopener noreferrer">here:</a>
+      You can see my source code <a href="https://github.com/philvanlane/astro-software-template" target="_blank" rel="noopener noreferrer">here.</a>
       <br></br>
-      You can see my documentation here: 
+      You can see my documentation <a href="https://philvanlane-astro-software-template.readthedocs.io/en/latest/" target="_blank" rel="noopener noreferrer">here.</a>
       </div>
     </div>
   );
